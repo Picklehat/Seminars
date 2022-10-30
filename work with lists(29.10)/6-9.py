c2c2278865c3ccb95c14a0d390ddea060178ca36
Vladislav Spirin <vlad.spirin.10@mail.ru>
2022-10-30 19:53:47 +0300

    fix folder name

--- work with lists(29.10)/6-9.py
+++ work with lists(29.10)/6-9.py
@@ -2,4 +2,4 @@ list_6 = [i**2 * -1 if i % 2 == 0 else i**2 for i in range(1, 11)]
 list_7 = []
 list_8 = [i if i % 2 == 1 else i%2+6*(i//2) for i in range(1, 12)] 
 list_9 = [] 
-print(list_8)
+print(list_9)
